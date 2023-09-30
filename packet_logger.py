1de9937636099b5eec286bbecff53eb24a77c49a
johnnycodes69 <59940172+johnnycodes69@users.noreply.github.com>
2023-09-30 17:40:50 -0400

    It works now and captures a lot of info

--- packet_logger.py
+++ packet_logger.py
@@ -1,19 +1,33 @@
 from scapy.all import *
-
 import logging
 from datetime import datetime
 
 # Configure logging
 logging.basicConfig(filename='packet_log.txt', level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 
-# Define a function to log packets
+# Logging funciton
 def log_packet(packet):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     packet_info = f"{timestamp} - Packet received: {len(packet)} bytes"
+    
+    if IP in packet:
+        packet_info += f", Source IP: {packet[IP].src}, Destination IP: {packet[IP].dst}"
+
+    # Get more stuff from each packet
+        if TCP in packet:
+            packet_info += f", Source Port: {packet[TCP].sport}, Destination Port: {packet[TCP].dport}, Protocol: TCP"
+        elif UDP in packet:
+            packet_info += f", Source Port: {packet[UDP].sport}, Destination Port: {packet[UDP].dport}, Protocol: UDP"
+        else:
+            packet_info += f", Protcol: {packet[IP].proto}"
+    
+    else:
+        packet_info += ", Non-IP packet"
+
     logging.info(packet_info)
 
-# Replace 'eth0' with your network interface name
-interface = 'eth0'
+# Replace 'Ethernet' with name of interface to monitor
+interface = 'Ethernet'
 
-# Capture packets and log them
+# Do the thing
 sniff(iface=interface, prn=lambda x: log_packet(x))
