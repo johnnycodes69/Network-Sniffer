29f9bed7d6a18053b667d2d801d63b668358de88
johnnycodes69 <59940172+johnnycodes69@users.noreply.github.com>
2023-09-30 17:13:24 -0400

    Delete fuck.bat

--- fuck.bat
+++
@@ -1,5 +0,0 @@
-@Echo Off
-For /F "Skip=1Delims=" %%a In (
-    '"WMIC NIC Where (Not NetConnectionStatus Is Null) Get NetConnectionID"'
-) Do For /F "Tokens=*" %%b In ("%%a") Do Echo=%%b
-Timeout -1
